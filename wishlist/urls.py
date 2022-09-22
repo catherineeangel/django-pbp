3cdb4433b05e1c8a75d4bcf065ec3e8b3a5b3bff
Catherine Angel <catheriner40540@gmail.com>
2022-09-22 08:51:31 +0700

    implement register, login, logout

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_wishlist_xml, show_wishlist_json, get_one_json, get_one_xml
+from wishlist.views import show_wishlist, show_wishlist_xml, show_wishlist_json, get_one_json, get_one_xml, register, login_user, logout_user 
 
 app_name = 'wishlist'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('xml/<int:id>', get_one_xml, name='get_one_xml'), 
     path('json/', show_wishlist_json, name='show_wishlist_json'), 
     path('json/<int:id>', get_one_json, name='get_one_json'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,15 +1,23 @@
-from django.http import HttpResponse
+import datetime
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from wishlist.models import BarangWishlist
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Catherine'
-}
 # Create your views here.
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Catherine',
+        'last_login': request.COOKIES['last_login'],
+    }
     return render(request, "wishlist.html", context)
 
 def show_wishlist_xml(request):
@@ -25,3 +33,37 @@ def get_one_xml(request, id):
 def get_one_json(request, id):
     data_satu_barang = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data_satu_barang), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('wishlist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('wishlist:login'))
+    response.delete_cookie('last_login')
+    return response
